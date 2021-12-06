95b9db8e5b9d2098fa145d6cf28286ed7a23bb99
Adrian <adrianlance86@gmail.com>
2021-12-05 22:35:06 -0800

    Jump and "crouch" made

--- Cat Alley/Assets/Scripts/PlayerController.cs
+++ Cat Alley/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float turnSpeed = 4.0f;
+    public float JumpForce = 2f;
     public float moveSpeed = 2.0f;
     public float minTurnAngle = -90.0f;
+    public float JumpTime = 1.5f;
     public float maxTurnAngle = 90.0f;
+    public Collider playerCollider;
     private float rotX;
     private float rotY;
+    private float jumpTimer;
+    private float gravity = -9.8f;
     public int fails = 0;
 
     private void Start() {
@@ -18,6 +23,7 @@ public class PlayerController : MonoBehaviour
     }
     void Update(){
         Aim();
+        Jump();
     }
     private void Aim(){
         rotY += Input.GetAxis("Mouse X") * turnSpeed;
@@ -32,16 +38,21 @@ public class PlayerController : MonoBehaviour
         Ray ray = Camera.main.ViewportPointToRay(rayOrigin);
         // debug Ray
         Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red);
-        //RaycastHit raycast_hit;
-        //var hit = Physics.Raycast(ray, out raycast_hit);
-        //var theTag = raycast_hit.collider.gameObject.tag;
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, rayLength))
         {
             if(Input.GetKeyDown(KeyCode.Mouse0) && hit.transform.gameObject.CompareTag("cat")) Destroy(hit.transform.gameObject);
         }
     }
-
+    private void Jump(){
+        if(Input.GetKeyDown(KeyCode.W) && jumpTimer <= 0){
+            jumpTimer = JumpTime;
+            GetComponent<Rigidbody>().velocity = new Vector3 (0.0f, JumpForce, 0.0f);
+        }
+        if(jumpTimer > 0){
+            jumpTimer -= Time.deltaTime;
+        }
+    }
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "hazard")
