659b91290e85ff610bc61ea9ae24a6f628320f37
Adrian <adrianlance86@gmail.com>
2021-12-05 19:54:56 -0800

    Raycasting installed

--- Cat Alley/Assets/PlayerController.cs
+++ Cat Alley/Assets/PlayerController.cs
@@ -16,10 +16,25 @@ public class PlayerController : MonoBehaviour
         Cursor.visible = false;
     }
     void Update(){
-        rotY += Input.GetAxis("Mouse X") * turnSpeed;
+        Aim();
+    }
+    private void Aim(){
+        rotY += Input.GetAxis("Mouse X") * turnSpeed;
         rotX += Input.GetAxis("Mouse Y") * -turnSpeed;
         rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
         rotY = Mathf.Clamp(rotY, minTurnAngle, maxTurnAngle);
         transform.eulerAngles = new Vector3(-rotX, rotY, 0);
+
+        Vector3 rayOrigin = new Vector3(0.5f, 0.5f, 0f); // center of the screen
+        float rayLength = 500f;
+        // actual Ray
+        Ray ray = Camera.main.ViewportPointToRay(rayOrigin);
+        // debug Ray
+        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, rayLength))
+        {
+            Debug.Log("Ray Working");
+        }
     }
 }
